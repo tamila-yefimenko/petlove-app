d4df891846bf26dc18606107c917080e168ff743
Tamila Yefimenko <tamilaedu88@gmail.com>
2026-02-05 14:35:09 +0200

    added background to thr AddPetPage

--- src/components/AddPet/AddPet.tsx
+++ src/components/AddPet/AddPet.tsx
@@ -1,9 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../Button/Button";
 import s from "./AddPet.module.css";
 
 const AddPet = () => {
+  const navigate = useNavigate();
+
   return (
-    <Button className={s.button}>
+    <Button
+      className={s.button}
+      onClick={() => {
+        navigate("/add-pet");
+      }}>
       Add Pet
       <svg className={s.icon}>
         <use href="/icons/sprite.svg#icon-plus" />
--- src/pages/AddPetPage/AddPetPage.tsx
+++ src/pages/AddPetPage/AddPetPage.tsx
@@ -1,5 +1,14 @@
+import { Container } from "../../components/Container/Container";
+import s from "./AddPetPage.module.css";
+
 const AddPetPage: React.FC = () => {
-  return <p> AddPetPage</p>;
+  return (
+    <Container className={s.addPetContainer}>
+      <div className={s.addPetPage}>
+        <div className={s.wrapper}></div>
+      </div>
+    </Container>
+  );
 };
 
 export default AddPetPage;
